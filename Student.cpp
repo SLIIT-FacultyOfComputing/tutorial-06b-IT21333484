dbd1d0414d58717c3b0cee6bf3b23c7d034af256
it21333484 <99311995+IT21333484@users.noreply.github.com>
2022-03-16 05:31:36 +0000

    Classes completed

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,19 @@
 #include "Student.h"
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 // Assign studentId and name
-Student::assignDetails() {
-  
+Student::assignDetails(int stdid, char sname[]) {
+    studentId = stdid;
+    strcpy(name,sname);
 }
 
 // Display StudentId and Name
 Student::display() {
+
+  cout<<"Student ID : "<<studentId<<endl;
+  cout<<"Name : "<<name<<endl;
   
 }
--- Student.h
+++ Student.h
@@ -1,9 +1,15 @@
 class Student {
   // private section
+  private:
+    int studentId;
+    char name[20];
   //    int studentId
   //    name <- 20 charcters
   
   // public section
+  public:
+    void assignDetails(int stdid, char sname[]);
+    void display();
   //    assignDetails() method declaration
   //    display() method declaration
 };
