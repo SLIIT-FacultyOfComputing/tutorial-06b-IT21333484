64d5834f8322efa969c6c9b1ed3669e946bb89a1
it21333484 <99311995+IT21333484@users.noreply.github.com>
2022-03-16 05:41:52 +0000

    Example 1 done

--- Student.cpp
+++ Student.cpp
@@ -1,17 +1,17 @@
 #include "Student.h"
 #include <iostream>
-#include <string>
+#include <string.h>
 
 using namespace std;
 
 // Assign studentId and name
-Student::assignDetails(int stdid, char sname[]) {
+void Student::assignDetails(int stdid, char sname[]) {
     studentId = stdid;
     strcpy(name,sname);
 }
 
 // Display StudentId and Name
-Student::display() {
+void Student::display() {
 
   cout<<"Student ID : "<<studentId<<endl;
   cout<<"Name : "<<name<<endl;
